f1b05fb48775ad3bab3e2f968aac82923d661b51
Luca Sartore <lucasartore02@gmail.com>
2026-02-04 11:20:48 +0100

    improove performance by reducing unecessary iterations of pf consensus

--- src/chasers_logic/constants.py
+++ src/chasers_logic/constants.py
@@ -4,7 +4,11 @@ NUMBER_OF_APPROXIMATION_COEFFICIENTS = (CHEBYSHEV_ORDER_X+1) * (CHEBYSHEV_ORDER_
 NUMBER_OF_PARTICLES = 2000
 NUMBER_OF_PARTICLES_TO_RESAMPLE_RANDOMLY = 400
 WEIGHTS_TO_ASSIGN_TO_RANDOMLY_SAMPLED_PARTICLES = 0.01
-CONSENSUS_ITERATIONS = 10
+# the number of consensus iteration should be higher
+# if the connection structure isn't a "mesh" however
+# if it is a mesh there is no advantage in increasing the number
+# as they will always converge in one iteration
+CONSENSUS_ITERATIONS = 1
 DEBUG = False
 
 NUM_EXTRA_PARTICLES_PER_SIDE = 25
